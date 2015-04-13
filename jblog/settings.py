8a201b4f7b5b3694851534fcd7a758b35b1dc723
Julian Trutmann <jtrutmann@gmail.com>
2015-04-13 15:21:07 -0400

    Changed database for deploy

--- jblog/settings.py
+++ jblog/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'jblog.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'easy': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'heroku': dj_database_url.config(),
+    'default': dj_database_url.config(),
     'postgres': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'de28o4933akbll',
